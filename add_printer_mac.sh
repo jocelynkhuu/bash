ff5eb8df3e95a3bbea4e9dd4296141a2b77f5fa2
jocelynkhuu <62637533+jocelynkhuu@users.noreply.github.com>
2021-07-29 10:57:43 -0400

    Create add_printer_mac.sh

---
+++ add_printer_mac.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+PRINTER_NAME="NAME_OF_PRINTER"
+PRINTER_LOCATION="LOCATION_OF_PRINTER"
+IP_ADDRESS="lpd://192.168.x.x"
+DRIVER="/System/Library/Frameworks/ApplicationServices.framework/Frameworks/PrintCore.framework/Resources/Generic.ppd"
+
+
+cat << "EOF"
+
+    ________________
+  _/_______________/|
+ /___________/___//||
+|===        |----| ||  I keep hearing music come from the printer. 
+|           |   �| ||  I think the paper is jammin'!
+|___________|   �| ||
+| ||/.�---.||    | ||
+|-||/_____\||-.  | |�
+|_||=L==H==||_|__|/ 
+
+EOF
+echo "Hello! Looks like you're here to add a printer!"
+read -p "Press enter/return to continue "
+echo "Adding printer!"
+echo ""
+if /usr/sbin/lpadmin -p ${PRINTER_NAME} -L ${PRINTER_LOCATION} -E -v ${IP_ADDRESS} -P ${DRIVER} ; then
+	echo ""
+	echo "Printer added!"
+	
+cat << "EOF"
+	
+	
+	 _    _     __      ________               _____ _____  ______       _______
+	| |  | |   /\ \    / /  ____|     /\      / ____|  __ \|  ____|   /\|__   __|
+	| |__| |  /  \ \  / /| |__       /  \    | |  __| |__) | |__     /  \  | |
+	|  __  | / /\ \ \/ / |  __|     / /\ \   | | |_ |  _  /|  __|   / /\ \ | |
+	| |  | |/ ____ \  /  | |____   / ____ \  | |__| | | \ \| |____ / ____ \| |
+	|_|  |_/_/    \_\/   |______| /_/    \_\  \_____|_|  \_\______/_/    \_\_|
+	
+	
+	 _____      __     ___
+	|  __ \   /\\ \   / / |
+	| |  | | /  \\ \_/ /| |
+	| |  | |/ /\ \\   / | |
+	| |__| / ____ \| |  |_|
+	|_____/_/    \_\_|  (_)
+	
+EOF
+	
+else
+	echo "Printer was not added :( Please try manually adding the printer instead. Please see xxxxx for more details"
+fi
+echo ""
+echo "Please close this window."
+
