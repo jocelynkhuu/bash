320accdce47de2858f5aabac04de86b369e695cd
jocelynkhuu <62637533+jocelynkhuu@users.noreply.github.com>
2021-10-22 22:07:55 -0400

    Delete jamf_recon.sh

--- jamf_recon.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Used to find logged in user and creates email to push into user & location field within JAMF
-
-loggedInUser=$( ls -la /dev/console | cut -d " " -f 4 )
-
-/usr/local/bin/jamf recon -endUsername $loggedInUser -email "$loggedInUser@petalcard.com"
