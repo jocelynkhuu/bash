3a9fdd0dcfc7fe7b9fd1614a14ddecce5867da46
jocelynkhuu <62637533+jocelynkhuu@users.noreply.github.com>
2021-10-22 22:08:11 -0400

    Create jamf_recon.sh

---
+++ jamf_recon.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Used to find logged in user and creates email to push into user & location field within JAMF
+
+loggedInUser=$( ls -la /dev/console | cut -d " " -f 4 )
+
+/usr/local/bin/jamf recon -endUsername $loggedInUser -email "$loggedInUser@email.com"
